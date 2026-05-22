98b2cd1a910cb98203e6c0b9f018ad8b25605f42
dxsprajapati <134995288+dxsprajapati@users.noreply.github.com>
2026-05-22 12:42:55 +0530

    Modify chat response to temporary message in Gujarati
    Updated the chat method to return a temporary response in Gujarati while the AI service is under development.

--- src/main/java/com/dxprajapati/ChatController.java
+++ src/main/java/com/dxprajapati/ChatController.java
@@ -18,9 +18,15 @@ public class ChatController {
 
     @PostMapping
     public String chat(@RequestBody ChatRequest request) {
+        //groqService.chat(request.getMessage());
+        String response = "હાલમાં હું મારી AI સર્વિસને વધુ સારી અને સ્માર્ટ બનાવવા માટે કામ કરી રહ્યો છું 😊
 
-        String response = groqService.chat(request.getMessage());
+એટલા માટે અત્યારે હું તમને AI આધારિત યોગ્ય જવાબ આપી શકતો નથી.
+થોડો સમય આપશો 🙏
 
+ખૂબ જલ્દી હું વધુ સારા અનુભવ સાથે ફરીથી તૈયાર થઈને તમને જાણ કરીશ 🚀
+
+— દક્ષેશ પ્રજાપતિ AI સર્વિસ"
 //        ChatLog log = new ChatLog(
 //                request.getMessage(),
 //                response,
